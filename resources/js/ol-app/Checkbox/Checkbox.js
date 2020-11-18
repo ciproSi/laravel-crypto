85d6c0e7986608e5387fa3a275d10172c6a79756
Šimon Cipro <simon.cipro@gmail.com>
2020-11-18 17:15:27 +0100

    uploading new route with checkboxes

--- resources/js/ol-app/Checkbox/Checkbox.js
+++ resources/js/ol-app/Checkbox/Checkbox.js
@@ -2,27 +2,28 @@ import React, { useState } from 'react'
 
 const Checkbox = (props) => {
     
-    const [checkboxes, setCheckboxes] = useState([
-        {id: 1, name: 'canicross', checked: false},
-        {id: 2, name: 'bikejoring', checked: false},
-        {id: 3, name: 'dogtrekking', checked: false}
-    ]);
+    // commented part only needed when living as standalone component
+    // const [checkboxes, setCheckboxes] = useState([
+    //     {id: 1, name: 'canicross', checked: false},
+    //     {id: 2, name: 'bikejoring', checked: false},
+    //     {id: 3, name: 'dogtrekking', checked: false}
+    // ]);
     
-    const handleChange = (e) => {
+    // const handleChange = (e) => {
         
-        const checkBoxIndex = checkboxes.findIndex((elm => elm.name == e.target.name));
+    //     const checkBoxIndex = checkboxes.findIndex((elm => elm.name == e.target.name));
         
-        checkboxes[checkBoxIndex].checked = !checkboxes[checkBoxIndex].checked
+    //     checkboxes[checkBoxIndex].checked = !checkboxes[checkBoxIndex].checked
 
-        setCheckboxes([...checkboxes]);
-    }
-
+    //     setCheckboxes([...checkboxes]);
+    // }
+    // const { checkboxes, handleChange } = props;
     return (
         <div className="checkbox-group">
             {
-                checkboxes.map((checkbox) => (
-                    <div className="form-group">
-                            <input key={ checkbox.id } type="checkbox" name={ checkbox.name } checked={ checkbox.checked } onChange={ handleChange } />
+                props.checkboxes.map((checkbox) => (
+                    <div key={ checkbox.id } className="form-group">
+                            <input type="checkbox" name={ checkbox.name } checked={ checkbox.checked } onChange={ props.handleChange } />
                             { checkbox.name }    
                     </div>
                 ))
--- resources/js/ol-app/NewRouteDetails/NewRouteDetails.js
+++ resources/js/ol-app/NewRouteDetails/NewRouteDetails.js
@@ -6,15 +6,31 @@ const NewRouteDetails = (props) => {
 
     const [activities, setActivities] = useState([]);
 
+    // get all available activities (routes are suitable for different activities) from API
     const fetchActivities = async () => {
         const response = await axios.get('/api/activities');
-
+        
+        // add property checked:false to array of activities objects
+        response.data.activities.forEach(activity => {
+            activity.checked = false;
+        });
+        
+        setActivities(response.data.activities);
     }
 
+    // fetch activities only once
     useEffect(() => {
         fetchActivities();
     }, []);
 
+    const handleCheckBoxChange = (e) => {
+        
+        const checkBoxIndex = activities.findIndex((elm => elm.name == e.target.name));
+        
+        activities[checkBoxIndex].checked = !activities[checkBoxIndex].checked;
+
+        setActivities([...activities]);
+    }
 
     return (
         <div className="form">
@@ -27,7 +43,9 @@ const NewRouteDetails = (props) => {
                     <label htmlFor="description">Description</label>
                     <textarea name="textarea" rows="8" cols="60"/>
                 </div>
-                <Checkbox />
+                <h3>Suitable for</h3>
+                {/* adds one checkbox for every activity */}
+                <Checkbox checkboxes={ activities} handleChange={ handleCheckBoxChange }/>
             </form>
         </div>
     )
